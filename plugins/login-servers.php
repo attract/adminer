5430dcb0ee347a1e05539963d9407771b7601d39
vadim <vad.pr.it@gmail.com>
2017-12-14 14:44:16 +0200

    Add table

--- plugins/login-servers.php
+++ plugins/login-servers.php
@@ -31,34 +31,73 @@ class AdminerLoginServers {
 	
 	function loginForm() {
 		?>
-<table cellspacing="0">
+<div style="width: 1500px">
+	<div style="float: left">
+		<table cellspacing="0">
 
+			<tr><th><?php echo lang('Server'); ?><td>
+			<select name="auth[server]" style="float: left;margin-right: 15px;">
+				<?php
+				foreach ($this->servers as $name=>$server){
+					?>
+					<option value="<?=$server['ip'] ?>" data-driver="<?=$server['driver']?>" ><?=$name ?></option>
+					<?
+				}
+				?>
+			</select>
+			<input name="local-mysql" type="button" value="Local Mysql" style="margin-right: 10px;">
+			<input name="local-pgsql" type="button" value="Local Pgsql">
 
+			<tr><th><?php echo lang('Driver'); ?><td>
+			<input name="auth[driver]" value="server" style="display: none;" >
+			<h4 id="selected_driver">MySQL</h4>
 
-	<tr><th><?php echo lang('Server'); ?><td>
-	<select name="auth[server]" style="float: left;margin-right: 15px;">
-		<?php
-		foreach ($this->servers as $name=>$server){
-			?>
-			<option value="<?=$server['ip'] ?>" data-driver="<?=$server['driver']?>" ><?=$name ?></option>
-			<?
-		}
-		?>
-	</select>
-	<input name="local-mysql" type="button" value="Local Mysql" style="margin-right: 10px;">
-	<input name="local-pgsql" type="button" value="Local Pgsql">
+			<tr><th><?php echo lang('Username'); ?><td><input id="username" name="auth[username]" value="<?php echo h($_GET["username"]);  ?>">
+			<tr><th><?php echo lang('Password'); ?><td><input type="password" name="auth[password]">
+		</table>
+		<p>
+				<div><? echo checkbox("auth[permanent]", 1, $_COOKIE["adminer_permanent"], lang('Permanent login')) . "\n"; ?></div>
+				<br>
+				<input type="submit" style="width: 300px;height: 30px;" value="<?php echo lang('Login'); ?>">
+		</p>
+	</div>
 
-	<tr><th><?php echo lang('Driver'); ?><td>
-	<input name="auth[driver]" value="server" style="display: none;" >
-	<h4 id="selected_driver">MySQL</h4>
-
-	<tr><th><?php echo lang('Username'); ?><td><input id="username" name="auth[username]" value="<?php echo h($_GET["username"]);  ?>">
-	<tr><th><?php echo lang('Password'); ?><td><input type="password" name="auth[password]">
-</table>
-<p>
-		<div><? echo checkbox("auth[permanent]", 1, $_COOKIE["adminer_permanent"], lang('Permanent login')) . "\n"; ?></div>
-		<br>
-		<input type="submit" style="width: 300px;height: 30px;" value="<?php echo lang('Login'); ?>">
+	<div style="margin-left: 10px; float: left">
+		<table>
+			<tr>
+				<th style="padding: 5px" colspan="5">Быстрое подключение</th>
+			</tr>
+			<tr>
+				<td><input type="submit" class="connect_to_db" value="Fivel db" style="background: yellowgreen; width: 100%"></td>
+				<td>Driver</td>
+				<td>3601</td>
+				<td colspan="2">user</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td><input type="text" value="" name="host" placeholder="Host"></td>
+				<td>
+					<select name="driver">
+						<option value="server" selected="">MySQL</option>
+						<option value="sqlite">SQLite 3</option>
+						<option value="sqlite2">SQLite 2</option>
+						<option value="pgsql">PostgreSQL</option>
+						<option value="oracle">Oracle</option>
+						<option value="mssql">MS SQL</option>
+						<option value="firebird">Firebird (alpha)</option>
+						<option value="simpledb">SimpleDB</option>
+						<option value="mongo">MongoDB (beta)</option>
+						<option value="elastic">Elasticsearch (beta)</option>
+					</select>
+				</td>
+				<td><input type="text" value="" name="port" placeholder="Port"></td>
+				<td><input type="text" value="" name="user" placeholder="User"></td>
+				<td><input type="text" value="" name="password" placeholder="Password"></td>
+				<td><input type="submit" class="connect_to_db" value="Добавить" ></td>
+			</tr>
+		</table>
+	</div>
+</div>
 <?php
 		return true;
 	}

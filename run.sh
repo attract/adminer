7ef2ab0eb0d286f21d307477789ab8e07ba1b6f6
vadim <vad.pr.it@gmail.com>
2020-03-19 12:41:50 +0200

    Added .env file

--- run.sh
+++ run.sh
@@ -7,5 +7,6 @@ docker cp ~/.ssh adminer_adminer-web_1:/root/.ssh
 docker exec -it adminer_adminer-web_1 bash -c "chmod 700 /root/.ssh/  && chmod 600 /root/.ssh/config && chown -R root.root /root/.ssh/"
 
 if ! [ -z $SSH_HOST ]
+
     then docker exec -it adminer_adminer-web_1 bash -c "ssh $SSH_HOST"
 fi
